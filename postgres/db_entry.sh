d4998cec028e5031e73215bfc14ad8f67d4c16c8
Shah Zobair <shah-zobair@users.noreply.github.com>
2018-05-18 12:48:24 -0400

    Update db_entry.sh

--- postgres/db_entry.sh
+++ postgres/db_entry.sh
@@ -2,3 +2,6 @@ echo "CREATE DATABASE testing;" | psql
 echo "CREATE TABLE dummy (name varchar(50), id int);" | psql testing
 echo "INSERT INTO dummy (name, id) values ('shah',1);" | psql testing
 echo "SELECT * FROM dummy;" | psql testing
+
+echo "CREATE ROLE shah with CREATEROLE login superuser PASSWORD 'shah123';" | psql
+echo "GRANT ALL PRIVILEGES ON DATABASE testing to shah;" | psql
