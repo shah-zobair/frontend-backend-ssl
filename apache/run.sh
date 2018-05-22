f07b96938d5c33513cecda8ed30cc77f53c16024
Shah Zobair <shah-zobair@users.noreply.github.com>
2018-05-22 16:09:39 -0400

    Update run.sh

--- apache/run.sh
+++ apache/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat /tmp/certs/postgresql.key > /tmp/certs/postgresql-user.key
+cat /tmp/certs/postgresql.key > /tmp/postgresql-user.key
 chmod 600 /tmp/postgresql-user.key
 
 cat /tmp/certs/frontend.key > /etc/pki/tls/private/frontend.key
