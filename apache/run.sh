57782102ec8ea9965b3bdeb44a074f769bdd918e
Shah Zobair <shah-zobair@users.noreply.github.com>
2018-05-22 11:10:54 -0400

    Update run.sh

--- apache/run.sh
+++ apache/run.sh
@@ -4,6 +4,6 @@ cat /tmp/postgresql.key > /tmp/postgresql-user.key
 chmod 600 /tmp/postgresql-user.key
 
 cat /tmp/frontend.key > /etc/pki/tls/private/frontend.key
-chmod 644 /etc/pki/tls/private/frontend.key
+chmod 600 /etc/pki/tls/private/frontend.key
 
 httpd -D FOREGROUND
